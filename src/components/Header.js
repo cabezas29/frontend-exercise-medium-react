ad85e0de9f47c78c483a88b85ef772a62267f358
Luis Cabezas <luiscabezas25@hotmail.com>
2020-07-13 07:22:18 -0500

    Creacion del Header sin estilos

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header() {
+  return(
+    <header className="header">
+        <article>
+            <h1>Ejercicio Web</h1>
+            <h3>Página creada con HTML5</h3>
+        </article>
+        <nav className="nav__menu">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Acerca de</a></li>
+                <li><a href="/">Portafolio</a></li>
+                <li><a href="/">Blog</a></li>
+                <li><a href="/">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header;
