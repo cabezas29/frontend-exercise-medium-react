5179ea7c00267a44da824ce85b3c14299dbfa957
Luis Cabezas <luiscabezas25@hotmail.com>
2020-07-13 17:12:19 -0500

    Se incluyo el HTML en la parte de final de la seccion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
+import Final from './components/Final'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Final />
     </div>
   );
 }
