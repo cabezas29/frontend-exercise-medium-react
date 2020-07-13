2617d5ae9f3cb98c5f5e23b21890558a3cc86531
Luis Cabezas <luiscabezas25@hotmail.com>
2020-07-13 18:33:15 -0500

    Agregando README.md

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-/* import './App.css'; */
 import Header from './components/Header'
 import Inicio from './components/Inicio'
 import Final from './components/Final'
